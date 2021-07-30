a84c1038b25d802ffdffe6726611d341ad6cb716
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2021-07-29 22:41:45 -0700

    Create Verbosity.java

---
+++ Verbosity.java
@@ -0,0 +1,12 @@
+HashMap<String, Integer> dict = new HasMap<String, Integer>()
+  dict.put("left", 1);
+  dict.put("right", 2);
+  dict.put("top", 3);
+  dict.put("bottom", 4);
+
+// you can also abbreviate in Java on an interview. It's fine if you can explain it
+HM<S, I> dict = new HM<S, I>()
+  dict.put("left", 1);
+... "right", 2
+... "top", 3
+... "bottom", 4
