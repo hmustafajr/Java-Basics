576f0c59b52107e1deadfc4db9a8d02d2925c060
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2024-03-23 11:03:02 -0700

    Update My First Java Program.java

--- My First Java Program.java
+++ My First Java Program.java
@@ -1,5 +1,4 @@
-//The main method is the starting point for all Java programsa
-
+// the main method is the starting point for all Java programs
 class MyClass [
     public static void main(string[] args) {
         System.out.println("Hello everyone")
